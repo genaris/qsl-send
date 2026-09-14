209de9c7ba97c09682dcd865fd2467529f6630bc
Ariel Gentile <gentilester@gmail.com>
2026-09-14 19:51:45 -0300

    ci: add release-please
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- qsl_send/__init__.py
+++ qsl_send/__init__.py
@@ -1,3 +1,3 @@
-"""qsl-send: generate and (later) e-mail QSL cards from an ADIF log."""
+"""qsl-send: generate and e-mail QSL cards from an ADIF log."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.0"  # x-release-please-version
